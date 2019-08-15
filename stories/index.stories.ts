20c0d0afd991ca64d9bdb49d27f0ccb03068863b
John Thomson <john_thomson@sil.org>
2019-08-15 01:41:27 -0500

    Made a story which displays an svg made with svg.js

--- stories/index.stories.ts
+++ stories/index.stories.ts
@@ -1,5 +1,6 @@
 import { document, console } from 'global';
 import { storiesOf } from '@storybook/html';
+import SVG from "svg.js";
 
 storiesOf('Demo', module)
   .add('heading', () => '<h1>Hello World</h1>')
@@ -10,3 +11,10 @@ storiesOf('Demo', module)
     button.addEventListener('click', e => console.log(e));
     return button;
   });
+storiesOf('svg', module)
+  .add('square', () => {
+    const div = document.createElement("div");
+    var draw = SVG(div).size(300,300);
+    var rect = draw.rect(100, 100).attr({ fill: '#f06' });
+    return div;
+  })
