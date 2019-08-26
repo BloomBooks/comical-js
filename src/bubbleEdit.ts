f2eaea9e9dd277e05d20ec8ab772d9df56df0fb4
John Thomson <john_thomson@sil.org>
2019-08-26 00:07:22 -0500

    Curve handle follows tip

--- src/bubbleEdit.ts
+++ src/bubbleEdit.ts
@@ -26,7 +26,15 @@ export default class BubbleEdit {
         }
         tool.onMouseDrag = (event: ToolEvent) => {
             if (state === "dragTip") {
+                const delta = event.point!.subtract(tipHandle.position!).divide(2);
                 tipHandle.position = event.point;
+                // moving the curve handle half as much is intended to keep
+                // the curve roughly the same shape as the tip moves.
+                // It might be more precise if we moved it a distance
+                // proportional to how close it is to the tip to begin with.
+                // Then again, we may decide to constrain it to stay
+                // equidistant from the root and tip.
+                curveHandle.position = curveHandle.position!.add(delta);
             } else if (state === "dragCurve") {
                 curveHandle.position = event.point;
             } else {
@@ -43,26 +51,32 @@ export default class BubbleEdit {
 
     static makeTail(root: Point, tip: Point, mid:Point): Path {
         const tailWidth = 50;
-        const xdiff = tip.x! - root.x!;
-        const ydiff = tip.y! - root.y!;
         // we want to make the base of the tail a line of length tailWidth
-        // at right angles to the line from root to tip
+        // at right angles to the line from root to mid
         // centered at root.
-        const angle = new Point(xdiff, ydiff).angle!;
-        const delta = new Point(0,0);
-        delta.angle = angle + 90;
-        delta.length = tailWidth / 4;
-        const begin = root.add(delta).add(delta);
-        const end = root.subtract(delta).subtract(delta);
-        const mid1 = mid.add(delta);
-        const mid2 = mid.subtract(delta);
-        const path1 = new Path.Arc(begin, mid1, tip);
+        const angleBase = new Point(mid.x! - root.x!, mid.y! - root.y!).angle!;
+        const deltaBase = new Point(0,0);
+        deltaBase.angle = angleBase + 90;
+        deltaBase.length = tailWidth / 2;
+        const begin = root.add(deltaBase);
+        const end = root.subtract(deltaBase);
+
+        // The midpoints of the arcs are a quarter base width either side of mid,
+        // offset at right angles to the root/tip line.
+        const angleMid = new Point(tip.x! - root.x!, tip.y! - root.y!).angle!;
+        const deltaMid = new Point(0,0);
+        deltaMid.angle = angleMid + 90;
+        deltaMid.length = tailWidth / 4;
+        const mid1 = mid.add(deltaMid);
+        const mid2 = mid.subtract(deltaMid);
+
+        const result = new Path.Arc(begin, mid1, tip);
         const path2 = new Path.Arc(tip, mid2, end);
-        path1.addSegments(path2.segments!);
+        result.addSegments(path2.segments!);
         path2.remove();
-        path1.strokeColor = new Color("black");
-        path1.fillColor = new Color("yellow");
-        return path1;
+        result.strokeColor = new Color("black");
+        result.fillColor = new Color("yellow");
+        return result;
     }
 
     static makeHandle(tip: Point): Path.Circle {
