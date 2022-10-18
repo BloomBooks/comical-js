9afe721820221623e20492c7afe177dd22509af4
John Thomson <john_thomson@sil.org>
2022-10-18 11:06:10 -0500

    Make rectangles thinner (BL-11618)

--- src/captionBubble.ts
+++ src/captionBubble.ts
@@ -3,18 +3,22 @@ import { Bubble } from "./bubble";
 
 // bubble: The bubble around which to make a caption
 // cornerRadii - Omit or pass undefined for square corners. For rounded corners, pass in the two radius amounts as a Size object.
-export function makeCaptionBox(bubble: Bubble, cornerRadii?: paper.Size): paper.Item {
+export function makeCaptionBox(bubble: Bubble, cornerRadii: paper.Size | undefined, thickness: number): paper.Item {
     const contentHolder = bubble.getDefaultContentHolder();
     const contentBounds = contentHolder.bounds;
 
-    const outline = makeOutline(contentBounds, cornerRadii);
+    const outline = makeOutline(contentBounds, cornerRadii, thickness);
 
     const result = new paper.Group([outline, contentHolder]);
     return result;
 }
 
-function makeOutline(bounds: paper.Rectangle, cornerRadii?: paper.Size): paper.Path.Rectangle {
-    const delta = 3;
+// The outline will be delta outside the bounds. Typically delta is the border thickness.
+function makeOutline(
+    bounds: paper.Rectangle,
+    cornerRadii: paper.Size | undefined,
+    delta: number
+): paper.Path.Rectangle {
     const outlineTopLeft = bounds.topLeft.subtract(delta);
     const outlineSize = new paper.Size(bounds.size.add(delta * 2));
 
